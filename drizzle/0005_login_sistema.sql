62788d1d0a25caf2e5c9911922355033aaaeaab4
duki-dodier <duki.dodier@gmail.com>
2026-08-03 20:18:43 -0300

    Make login migration compatible with hosted SQLite

--- drizzle/0005_login_sistema.sql
+++ drizzle/0005_login_sistema.sql
@@ -47,15 +47,16 @@ LIMIT 1;
 
 CREATE UNIQUE INDEX IF NOT EXISTS "Funcionario_usuario_key" ON "Funcionario"("usuario");
 
-INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo")
-SELECT f."id", p."processo"
-FROM "Funcionario" f
-CROSS JOIN (
-  SELECT 'PRODUCAO' AS "processo" UNION ALL SELECT 'CORTE' UNION ALL SELECT 'BATIDA'
-  UNION ALL SELECT 'FURACAO' UNION ALL SELECT 'DOBRA' UNION ALL SELECT 'AMASSAR'
-  UNION ALL SELECT 'CORTE_GRAU' UNION ALL SELECT 'LIXAR' UNION ALL SELECT 'SOLDAGEM'
-  UNION ALL SELECT 'AGRUPAR'
-) p
-WHERE f."usuario" = 'tiago';
+-- Instrucoes separadas evitam o limite de SELECT composto do SQLite no deploy.
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'PRODUCAO' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'CORTE' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'BATIDA' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'FURACAO' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'DOBRA' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'AMASSAR' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'CORTE_GRAU' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'LIXAR' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'SOLDAGEM' FROM "Funcionario" WHERE "usuario" = 'tiago';
+INSERT OR IGNORE INTO "FuncionarioProcesso" ("funcionarioId", "processo") SELECT "id", 'AGRUPAR' FROM "Funcionario" WHERE "usuario" = 'tiago';
 
 PRAGMA optimize;
